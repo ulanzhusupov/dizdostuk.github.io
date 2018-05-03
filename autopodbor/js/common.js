c2864951fc62ec4caf2b9b8c359dabd7af4e8a04
Ulanbek Zhusupov <diz.dostuk@yandex.ru>
2018-05-04 01:05:24 +0300

    added menu for devices

---
+++ autopodbor/js/common.js
@@ -0,0 +1,39 @@
+$(function() {
+
+	$('.menu-link').click(function(){
+		$('.menu-link').toggleClass('menu-link_active');//для меню иконки
+		$('.menu-devices').toggleClass('menu-devices_active');//сама меню при разреш. меньше 992px
+	});
+
+	$('.menu-link_active').click(function() {
+		$('.menu-link').toggleClass('menu-link_active');
+	});
+
+	$('.menu-devices a').click(function() {
+		$('.menu-devices').toggleClass('menu-devices_active');
+	});
+
+	$('.sliders').bxSlider();
+
+	$("a[href^='#']").click(function(){
+		var _href = $(this).attr("href");
+
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+
+		return false;
+	});
+
+	// По стандарту
+    jQuery.scrollSpeed(100, 800);
+    // Собственная анимация при прокрутке 
+    jQuery.scrollSpeed(100, 800, 'easeOutCubic');
+    
+    
+    
+
+	
+	
+
+});
+
+
