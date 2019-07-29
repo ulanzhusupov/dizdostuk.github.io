e8b06a6c4683ad8e1643a7894be2261bab3dd95d
Ulanbek Zhusupov <diz.dostuk@yandex.ru>
2019-07-29 08:10:51 +0300

    Tried add touchmove event listener, but not worked

--- verticalslider/app/js/main.js
+++ verticalslider/app/js/main.js
@@ -1,6 +1,16 @@
+import SimpleParallax from "./simpleParrallax/simpleParallax";
 var image = document.getElementsByClassName('vertical_slider__content');
-new simpleParallax(image, {
+new SimpleParallax(image, {
 	delay: .2,
 	transition: 'cubic-bezier(0,0,0,1)'
 });
 
+let sliderWrap = document.getElementById('vertical_slider--wrapper');
+let sliderContainer = document.getElementById('vertical_slider');
+
+sliderContainer.addEventListener('touchmove', (e) => {
+	for(let i = 0; i < e.touches.length; i++) {
+		let touch = e.touches[i];
+		console.log(touch)
+	}
+}, false);
